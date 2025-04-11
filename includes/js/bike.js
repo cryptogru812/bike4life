0689883fad2421398a4e4200849ed419f0f19ca1
cryptogru812 <wongaiden812@gmail.com>
2025-04-11 08:12:36 -0700

    Update

--- includes/js/bike.js
+++ includes/js/bike.js
@@ -6,18 +6,18 @@ const zoll = document.getElementById("zoll");
 const tools = document.getElementById("tools");
 
 const bikes = [
-    { id: 1, image: './includes/image/bike/bike1.svg', title: 'Aristos T', slogan: 'A titan for everyone!', price: 2000 },
-    { id: 2, image: './includes/image/bike/bike2.svg', title: 'Aristos GT', slogan: 'For gravel and bikepacking', price: 3500 },
-    { id: 3, image: './includes/image/bike/bike3.svg', title: 'Aristos GTR', slogan: 'One wheel - infinite possibilities', price: 9000 },
-    { id: 4, image: './includes/image/bike/bike4.svg', title: 'Aristos GTR', slogan: 'One wheel - infinite possibilities', price: 8000 },
-    { id: 5, image: './includes/image/bike/bike1.svg', title: 'Aristos T', slogan: 'A titan for everyone!', price: 7500 },
-    { id: 6, image: './includes/image/bike/bike2.svg', title: 'Aristos GT', slogan: 'For gravel and bikepacking', price: 3200 },
-    { id: 7, image: './includes/image/bike/bike3.svg', title: 'Aristos GTR', slogan: 'One wheel - infinite possibilities', price: 3800 },
-    { id: 8, image: './includes/image/bike/bike4.svg', title: 'Aristos GTR', slogan: 'One wheel - infinite possibilities', price: 3400 },
-    { id: 9, image: './includes/image/bike/bike1.svg', title: 'Aristos T', slogan: 'A titan for everyone!', price: 7300 },
-    { id: 10, image: './includes/image/bike/bike2.svg', title: 'Aristos GT', slogan: 'For gravel and bikepacking', price: 6000 },
-    { id: 11, image: './includes/image/bike/bike3.svg', title: 'Aristos GTR', slogan: 'One wheel - infinite possibilities', price: 5400 },
-    { id: 12, image: './includes/image/bike/bike4.svg', title: 'Aristos GTR', slogan: 'One wheel - infinite possibilities', price: 4700 },
+    { id: 1, image: '../includes/image/bike/bike1.svg', title: 'Aristos T', slogan: 'A titan for everyone!', price: 2000 },
+    { id: 2, image: '../includes/image/bike/bike2.svg', title: 'Aristos GT', slogan: 'For gravel and bikepacking', price: 3500 },
+    { id: 3, image: '../includes/image/bike/bike3.svg', title: 'Aristos GTR', slogan: 'One wheel - infinite possibilities', price: 9000 },
+    { id: 4, image: '../includes/image/bike/bike4.svg', title: 'Aristos GTR', slogan: 'One wheel - infinite possibilities', price: 8000 },
+    { id: 5, image: '../includes/image/bike/bike1.svg', title: 'Aristos T', slogan: 'A titan for everyone!', price: 7500 },
+    { id: 6, image: '../includes/image/bike/bike2.svg', title: 'Aristos GT', slogan: 'For gravel and bikepacking', price: 3200 },
+    { id: 7, image: '../includes/image/bike/bike3.svg', title: 'Aristos GTR', slogan: 'One wheel - infinite possibilities', price: 3800 },
+    { id: 8, image: '../includes/image/bike/bike4.svg', title: 'Aristos GTR', slogan: 'One wheel - infinite possibilities', price: 3400 },
+    { id: 9, image: '../includes/image/bike/bike1.svg', title: 'Aristos T', slogan: 'A titan for everyone!', price: 7300 },
+    { id: 10, image: '../includes/image/bike/bike2.svg', title: 'Aristos GT', slogan: 'For gravel and bikepacking', price: 6000 },
+    { id: 11, image: '../includes/image/bike/bike3.svg', title: 'Aristos GTR', slogan: 'One wheel - infinite possibilities', price: 5400 },
+    { id: 12, image: '../includes/image/bike/bike4.svg', title: 'Aristos GTR', slogan: 'One wheel - infinite possibilities', price: 4700 },
 ]
 
 const filterBikesByPrice = () => {
@@ -26,13 +26,13 @@ const filterBikesByPrice = () => {
     const rangeMaxElem = document.getElementById("range-max");
     const minPrice = parseInt(rangeMinElem.value) || 0;
     const maxPrice = parseInt(rangeMaxElem.value) || 10000;
-    
+
     // Return only bikes with price between min and max
     return bikes.filter(bike => bike.price >= minPrice && bike.price <= maxPrice);
 };
 
-const displayBikes  = () => {   
-    
+const displayBikes = () => {
+
     const filteredBikes = filterBikesByPrice();
 
     bikeCrad.innerHTML = "";
@@ -46,7 +46,7 @@ const displayBikes  = () => {
         div.innerHTML = `
         <div class="bike-card">
             <div class="bike-header">
-                <img src="./assets/image/logo.png" alt="Falkenjagd Logo" class="logo">
+                <img src="../includes/image/logo.png" alt="Falkenjagd Logo" class="logo">
                 <span class="brand-name">Falkenjagd</span>
             </div>
             <div class="bike-image">
@@ -54,47 +54,47 @@ const displayBikes  = () => {
             </div>
 
             <div class="bike-footer">
-                <div class="bike-info">
-                <h3>${bike.title}</h3>
+                <div class="brand">
+                    <div class="bike-info">
+                        <h3>${bike.title}</h3>
+                    </div>
+                    <div class="compare">
+                        <input type="checkbox" id="compare${bike.id}">
+                        <label for="compare${bike.id}">Compare</label>
+                    </div>
+                </div>
                 <p><em>${bike.slogan}</em></p>
-                </div>
-                <div class="compare">
-                <input type="checkbox" id="compare${bike.id}">
-                <label for="compare${bike.id}">Compare</label>
-                </div>
             </div>
         </div>
       `;
-      bikeCrad.appendChild(div);
+        bikeCrad.appendChild(div);
     });
     filteredBikes.forEach(() => {
         const div = document.createElement("div");
         div.innerHTML = `
         <div class="price-box">
-            <div class="price">3.499,00€</div>
+            <div class="price-rev">3.499,00€</div>
             <button class="explore-btn">Explore</button>
             <div class="divider"></div>
             <div class="price">1.890,00€</div>
-            <button class="explore-btn">Explore</button>
+            <button class="explore-btn-2">Explore</button>
             <div class="divider"></div>
             <ul class="features">
-                <li class="positive"><span>+</span> Excellent price-performance ratio</li>
+                <li class="positive" style="padding-top: 43px; padding-bottom: 20px;"><span>+</span> Excellent price-performance ratio</li>
                 <li class="positive"><span>+</span> Minimalistically displays everything that is required</li>
                 <li class="negative"><span>–</span> No complete cable integration possible</li>
             </ul>
         </div>
       `;
-      priceBox.appendChild(div);
+        priceBox.appendChild(div);
     });
     filteredBikes.forEach(() => {
         const div = document.createElement("div");
         div.innerHTML = `
         <div class="card">
             <div class="icon">
-                <img src="./assets/image/bike-frame-icon.svg" alt="Bike Frame" />
+                <img src="../includes/image/bike-frame-icon.svg" alt="Bike Frame" />
             </div>
-            <div class="label">Titan 3/2,5</div>
-            <div class="divider"></div>
             <div class="bar-container">
                 <div class="bar-comfortable"></div>
                 <div class="bar-sporty"></div>
@@ -103,7 +103,8 @@ const displayBikes  = () => {
                 <span>Comfortable</span>
                 <span>Sporty</span>
             </div>
-
+            <div class="divider"></div>
+            <div class="label">Titan 3/2,5</div>
             <div class="divider"></div>
             <div class="label">double butted</div>
             <div class="divider"></div>
@@ -120,32 +121,32 @@ const displayBikes  = () => {
             <div class="label">Partially integrated from down tube</div>
         </div>
       `;
-      titan.appendChild(div);
+        titan.appendChild(div);
     });
     filteredBikes.forEach(() => {
         const div = document.createElement("div");
         div.innerHTML = `
         <div class="card">
             <div class="bsa">
-                <img src="./assets/image/bsa.svg" alt="BSA" />
+                <img src="../includes/image/bsa.svg" alt="BSA" />
             </div>
-            <div class="label">BSA</div>
+            <div class="first-label">BSA</div>
             <div class="divider"></div>
             <div class="label">Chain , single and double</div>
             <div class="divider"></div>
             <div class="label">10 - 48</div>
         </div>
       `;
-      bsa.appendChild(div);
+        bsa.appendChild(div);
     });
     filteredBikes.forEach(() => {
         const div = document.createElement("div");
         div.innerHTML = `
         <div class="card">
             <div class="zoll">
-                <img src="./assets/image/zoll.svg" alt="zoll" />
+                <img src="../includes/image/zoll.svg" alt="zoll" />
             </div>
-            <div class="label">28 Zoll</div>
+            <div class="first-label">28 Zoll</div>
             <div class="divider"></div>
             <div class="label">--</div>
             <div class="divider"></div>
@@ -154,16 +155,16 @@ const displayBikes  = () => {
             <div class="label">145Kg</div>
         </div>
       `;
-      zoll.appendChild(div);
+        zoll.appendChild(div);
     });
     filteredBikes.forEach(() => {
         const div = document.createElement("div");
         div.innerHTML = `
         <div class="card">
             <div class="zoll">
-                <img src="./assets/image/tools.svg" alt="tools" />
+                <img src="../includes/image/tools.svg" alt="tools" />
             </div>
-            <div class="dot"><div class="cyan"></div></div>
+            <div class="dot"><div class="first-cyan"></div></div>
             <div class="divider"></div>
             <div class="dot"><div class="pink"></div></div>
             <div class="divider"></div>
@@ -176,7 +177,7 @@ const displayBikes  = () => {
             <div class="dot"><div class="pink"></div></div>
         </div>
       `;
-      tools.appendChild(div);
+        tools.appendChild(div);
     });
     bikeCrad.addEventListener("scroll", () => {
         priceBox.scrollLeft = bikeCrad.scrollLeft;
