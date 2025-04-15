7b74f841a875a12f5c79c012f49c0d184355d481
cryptogru812 <wongaiden812@gmail.com>
2025-04-15 01:22:41 -0700

    add sliderbar

---
+++ includes/js/scroll.js
@@ -0,0 +1,61 @@
+const slider = document.getElementById('bike-card');
+const range = document.getElementById('progressSlider');
+const range1 = document.getElementById('range-min');
+const range2 = document.getElementById('range-max');
+const customSlider = document.getElementById('custom-slider');
+
+// Update range when scrolling
+slider.addEventListener('scroll', () => {
+    const maxScroll = slider.scrollWidth - slider.clientWidth;
+    const percent = (slider.scrollLeft / maxScroll) * 100;
+    range.value = percent;
+});
+
+// Scroll slider when dragging range input
+range.addEventListener('input', () => {
+    const maxScroll = slider.scrollWidth - slider.clientWidth;
+    const newScrollLeft = (range.value / 100) * maxScroll;
+    slider.scrollLeft = newScrollLeft;
+});
+
+function scrollSlider(direction) {
+    const pitch = window.innerWidth > 768 ? 200 : 50; 
+    const scrollAmount = slider.querySelector('img').offsetWidth + pitch;
+    slider.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
+}
+
+range1.addEventListener('input', () => {
+    const a = window.innerWidth > 768 ? 0.8544 : 0.8;
+    if(slider.scrollWidth < window.innerWidth * a) {
+        customSlider.style.display = 'none';
+    }
+    else {
+        customSlider.style.display = 'block';
+    }
+  });
+
+range2.addEventListener('input', () => {
+    const a = window.innerWidth > 768 ? 0.8544 : 0.8;
+    if(slider.scrollWidth < window.innerWidth * a) {
+        customSlider.style.display = 'none';
+    }
+    else {
+        customSlider.style.display = 'block';
+    }
+});
+
+window.addEventListener('load', () => {
+    const a = window.innerWidth > 768 ? 0.8544 : 0.8;
+    if(slider.scrollWidth < window.innerWidth * a) {
+        customSlider.style.display = 'none';
+    }
+    else {
+        customSlider.style.display = 'block';
+    } 
+})
+
+
+
+
+
+
